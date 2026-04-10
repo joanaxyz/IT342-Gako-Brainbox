a897f7c9e2cbca41ec6e036cc89d2e6d4c9b36a3
jian <joanacarlagako15@gmail.com>
2026-04-11 00:28:13 +0800

    update: web config

--- backend/src/main/java/edu/cit/gako/brainbox/auth/config/WebConfig.java
+++ backend/src/main/java/edu/cit/gako/brainbox/auth/config/WebConfig.java
@@ -25,7 +25,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*")
+                .allowedOriginPatterns(
+                        "http://localhost:*",
+                        "http://127.0.0.1:*",
+                        "http://appassets.androidplatform.net"
+                )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
